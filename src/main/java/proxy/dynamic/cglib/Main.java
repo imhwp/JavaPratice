b80e4fe4b7ba537e5b330a6e96f60f50695bd2be
hank.huang <hank.huang@zoom.us>
2021-07-08 18:26:48 +0800

    cglib note

--- src/main/java/proxy/dynamic/cglib/Main.java
+++ src/main/java/proxy/dynamic/cglib/Main.java
@@ -1,5 +1,10 @@
 package proxy.dynamic.cglib;
 
+/**
+ * 定义一个类；
+ * 自定义 MethodInterceptor 并重写 intercept 方法，intercept 用于拦截增强被代理类的方法，和 JDK 动态代理中的 invoke 方法类似；
+ * 通过 Enhancer 类的 create()创建代理类；
+ */
 public class Main {
     public static void main(String[] args) {
         AliSms aliSms = (AliSms) CglibProxyFactory.getProxy(AliSms.class);
